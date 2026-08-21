3a1230ecf658aaa3bcf9361259e6f74b9baa65df
xiomaramoncayo263-hash <xiomaramoncayo263@gmail.com>
2026-08-21 16:44:55 -0500

    Corrección de errores.

--- middlewares/fisica.middlewares.js
+++ middlewares/fisica.middlewares.js
@@ -2,9 +2,9 @@
 export const validarVelocidad = (req, res, next) => {
     const { distancia, tiempo } = req.body;
 
-    if (tiempo <= 0) {
+    if (tiempo === undefined || distancia === undefined) {
         return res.status(400).json({
-            "mensaje": "El valor de tiempo debe ser mayor a cero"
+            "mensaje": "Datos incompletos"
         });
     };
 
@@ -14,20 +14,22 @@ export const validarVelocidad = (req, res, next) => {
         });
     };
 
-    if (tiempo === undefined || distancia === undefined) {
+    if (tiempo <= 0) {
         return res.status(400).json({
-            "mensaje": "Datos incompletos"
+            "mensaje": "El valor de tiempo debe ser mayor a cero"
         });
     };
+    
     next();
 }
 
 // validación de distancia
 export const validarDistancia = (req, res, next) => {
     const { velocidad, tiempo } = req.body;
-    if (tiempo < 0 || velocidad < 0) {
+
+    if (velocidad === undefined || tiempo === undefined) {
         return res.status(400).json({
-            "mensaje": "Tiempo y velocidad deben ser numeros positivos"
+            "mensaje": "Datos incompletos"
         });
     };
 
@@ -37,43 +39,47 @@ export const validarDistancia = (req, res, next) => {
         });
     };
 
-    if (velocidad === undefined || tiempo === undefined) {
+    if (tiempo < 0 || velocidad < 0) {
         return res.status(400).json({
-            "mensaje": "Datos incompletos"
+            "mensaje": "Tiempo y velocidad deben ser numeros positivos"
         });
     };
+
     next();
 }
 
 // validación de tiempo
 export const validarTiempo = (req, res, next) => {
-    const { velocidad, tiempo } = req.body;
+    const { velocidad, distancia } = req.body;
+
+    if (velocidad === undefined || distancia === undefined) {
+        return res.status(400).json({
+            "mensaje": "Datos incompletos"
+        });
+    };
+
+    if (typeof velocidad !== "number" || typeof distancia !== "number") {
+        return res.status(400).json({
+            "mensaje": "Los valores deben ser numericos"
+        });
+    };
+
     if (velocidad <= 0) {
         return res.status(400).json({
             "mensaje": "El valor de velocidad debe ser mayor a 0"
         });
     };
 
-    if (typeof velocidad !== "number" || typeof tiempo !== "number") {
-        return res.status(400).json({
-            "mensaje": "Los valores deben ser numericos"
-        });
-    };
-
-    if (velocidad === undefined || tiempo === undefined) {
-        return res.satatus(400).json({
-            "mensaje": "Datos incompletos"
-        });
-    };
     next();
 }
 
 // validaciones de fuerza
 export const validarFuerza = (req, res, next) => {
     const { masa, aceleracion } = req.body;
-    if (masa < 0 || aceleracion < 0) {
+
+    if (masa === undefined || aceleracion === undefined) {
         return res.status(400).json({
-            "mensaje": "Los valores deben ser positivos"
+            "mensaje": "Datos incompletos"
         });
     };
 
@@ -83,20 +89,22 @@ export const validarFuerza = (req, res, next) => {
         });
     };
 
-    if (masa === undefined || aceleracion === undefined) {
+    if (masa < 0 || aceleracion < 0) {
         return res.status(400).json({
-            "mensaje": "Datos incompletos"
+            "mensaje": "Los valores deben ser positivos"
         });
     };
+
     next();
 }
 
 // validación de peso 
 export const validarPeso = (req, res, next) => {
     const { masa } = req.body;
-    if (masa < 0) {
+
+    if (masa === undefined) {
         return res.status(400).json({
-            "mensaje": "Los valores deben ser positivos"
+            "mensaje": "Datos incompletos"
         });
     };
 
@@ -106,33 +114,36 @@ export const validarPeso = (req, res, next) => {
         });
     };
 
-    if (masa === undefined) {
-        return res.status(400).json({
-            "mensaje": "Datos incompletos"
-        });
-    };
-    next();
-}
-
-// validación de energia conetica
-export const validarEnergiaCinetica = (req, res, next) => {
-    const { masa, velocidad } = req.body;
-    if (masa < 0 || velocidad < 0) {
+    if (masa < 0) {
         return res.status(400).json({
             "mensaje": "Los valores deben ser positivos"
         });
     };
+    
+    next();
+}
 
+// validación de energia cinetica
+export const validarEnergiaCinetica = (req, res, next) => {
+    const { masa, velocidad } = req.body;
+    
+    if (masa === undefined || velocidad === undefined) {
+        return res.status(400).json({
+            "mensaje": "Datos incompletos"
+        });
+    };
+    
     if (typeof masa !== "number" || typeof velocidad !== "number") {
         return res.status(400).json({
             "mensaje": "Los valores deben ser numericos"
         });
     };
 
-    if (masa === undefined || velocidad === undefined) {
+    if (masa < 0 || velocidad < 0) {
         return res.status(400).json({
-            "mensaje": "Datos incompletos"
+            "mensaje": "Los valores deben ser positivos"
         });
     };
+
     next();
 }
